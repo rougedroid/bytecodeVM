b4ba1e609f6042b99486bd42ae749d477fa81b96
Bhavan <bhavan@tutamail.com>
2026-05-27 15:02:29 +0530

    Added a converter from text commands to literal binary. This is the expected output of compiler, and the actual input of VM. This predefined input is for testing purposes of the VM only.

--- vm_main.c
+++ vm_main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
-typedef enum(
+typedef enum{
   WRITE_CONST_INT = 0x0020, // OPCODE [REG] [Value] Value is 16 bits so 15 bits of signed int. INT Range = -32768 to +32768.
   OP_NONE  = 0x0000, // SKIP
   OP_RETURN = 0x0022, // OPCODE [REG] Print out [REG]
@@ -12,7 +13,7 @@ typedef enum(
   LOAD_REG = 0x0023, // OPCODE [REG] 
   OP_SUB = 0x0026, // OPCODE [REG 1] [REG 2] --> again put in default reg. have 1 bit for carry. 
   OP_JUMP = 0x0027, // OPCODE [REG]
-)Opcodes;
+  }Opcodes;
 
 // We will have it act like a chip only. we will have all the input also "take" us chips space. basically all the code is stored in data lines. 
 //
@@ -38,8 +39,35 @@ int main(){
   // OP_SUB gives out at --> 0x0002 and Sets REMAINDER BIT ON 0x0003
   //
   // 
-  
-  in_commands = "[WRITE_CONST_INT][0x0104][0x0005][WRITE_CONST_INT][0x0105][0x0007][OP_ADD][0x0104][0x0105][OP_LOAD_REG][0x0001][x0104][WRITE_CONST_INT][0X0105][0X0003][OP_ADD][0X0105][0X0104][OP_LOAD_REG][0x0001][0x0106]";
-  
+  char in_commands[] = "[WRITE_CONST_INT][0x0104][0x0005][WRITE_CONST_INT][0x0105][0x0007][OP_ADD][0x0104][0x0105][OP_LOAD_REG][0x0001][x0104][WRITE_CONST_INT][0X0105][0X0003][OP_ADD][0X0105][0X0104][OP_LOAD_REG][0x0001][0x0106]";
+  const char delimeters[] = "[]";
+  char *in_commands_bin[336];
+  char *token = strtok(in_commands, delimeters);
+  int instruction_len = 0;
+  while (token!=NULL){
+    if (strcmp(token, "WRITE_CONST_INT"))  {
+      strcpy(*(in_commands_bin+instruction_len), "0000000000100000");
+    }else if(strcmp(token, "OP_NONE"))  {
+      strcpy(*(in_commands_bin+instruction_len), "0000000000000000");
+    }else if( strcmp(token, "OP_RETURN")){
+      strcpy(*(in_commands_bin+instruction_len), "0000000000100010");
 
+    }else if (strcmp(token, "OP_ADD")) {
+      strcpy(*(in_commands_bin+instruction_len), "0000000000100011");
+    }else if (strcmp(token, "OP_LOAD_REG")) {
+      strcpy(*(in_commands_bin+instruction_len), "0000000000100101");
+     
+    }else if (strcmp(token, "OP_SUB")) {
+      strcpy(*(in_commands_bin+instruction_len), "0000000000100110");
+    
+    }else if (strcmp(token, "OP_JUMP")) {
+      strcpy(*(in_commands_bin+instruction_len), "0000000000100111");
+    }else {
+      strcpy(*(in_commands_bin+instruction_len), token);
+    } 
+    
+
+    instruction_len+=8;
+  }
+  printf("%s \n", in_commands_bin);
 }
