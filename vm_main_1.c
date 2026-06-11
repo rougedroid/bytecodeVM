487174b2ab3382483a31e8a7bd3cadf6a1a0e811
Bhavan <bhavan@tutamail.com>
2026-06-11 08:15:23 +0530

    FIxed inconsistensies in data types. changed data blocks to uint16 so we directly read 2 bytes at a time. Allaccess points also changed to use uint16 system. Tested on 2 byte numbers and test worked. Further testing required ig. Only tested existing program that checks if two numbers are equal and prints 3 if they are. and 4 if they aren't

--- vm_main_1.c
+++ vm_main_1.c
@@ -6,7 +6,7 @@
 #include <inttypes.h>
 #include <stdint.h>
 
-uint8_t * datablocks;
+uint16_t * datablocks;
 uint8_t * outputbuffer;
 
 
@@ -15,7 +15,10 @@ uint8_t * outputbuffer;
 // 
 // Output Buffer address 0xFFFE - 0xFFFF --> Last 8 bits
 // Default output and error buffers etc etc from 0xFFF0 -> 0xFFF7
-// Addition Result: 0xFFF1 
+// Addition Result: 0xFFF1 and 0xFFF2 
+// Subtraction Result: 0xFFF3 and 0xFFF4 
+// Overflow Bit: 0xFFF5 and 0xFFF6
+// Multiplication Result:  
 //
 
 typedef enum{
@@ -31,11 +34,13 @@ typedef enum{
   OP_JMP_RELP = 0x0029, // OPCODE [VALUE] --> Goes forward by value number of bytes. (value must be even number) 
   OP_JMP_RELN = 0x0030, // OPCODE [VALUE] --> Goes back by value number of bytes. (value must be even)
   OP_CMP_JMP = 0x0031, // OPCODE [REG1] [REG2] [JMP] --> jumps JMP number of bytes forward. ( JMP must be even )
+  OP_MUL = 0X0032, // OPCODE [REG1] [REG2] --> Stored in 0xFFF7 and 0xFFF8
+  OP_DIV = 0x0033, // OPCODE [REG1] [REG2] --> Store quotent in 0xFFF9 and 0xFFFA ; Remainder in 0xFFFB and 0xFFFC;
+
 }Opcodes;
 // Note to self: A future reimplementation is required. Right now, there are too many arbitrary constraints for this to be a VM. In future, simulate a real chip from datasheet. And encode those constraints and implement clock cycles also. 
-//
 uint16_t test_values[] = {
-  WRITE_CONST_INT, 0x0104, 0x0007,OP_RETURN, 0x0104, WRITE_CONST_INT, 0x0105, 0x0005, OP_RETURN, 0x0105, OP_CMP_JMP, 0x0104, 0x0105, 0x000c, WRITE_CONST_INT, 0x0106, 0x0004, OP_JMP_RELP, 0x0006, WRITE_CONST_INT, 0x0106, 0x0003, OP_RETURN, 0x0106
+  WRITE_CONST_INT, 0x0104, 0x0105,OP_RETURN, 0x0104, WRITE_CONST_INT, 0x0105, 0x0105, OP_RETURN, 0x0105, OP_CMP_JMP, 0x0104, 0x0105, 0x000c, WRITE_CONST_INT, 0x0106, 0x0004, OP_JMP_RELP, 0x0006, WRITE_CONST_INT, 0x0106, 0x0003, OP_RETURN, 0x0106
 };
 // rn the integers accepted are 16 bits. i.e. 2 bytes, but we process only single byte integers. 
 // Option 1: make it 1 byte integers -> horrible for everything will have to redesign everything. 
@@ -62,7 +67,7 @@ int main(){
   //op = *(test_values)[0]
   for (int i = 0; i < instruction_len; i++){
     //memcpy(&op, datablocks + i*2, 2); // Skipping 1 i for returning buffer is fine, cuz they are 16 bit address so like an address value also takes up 2 bytes. so does an opcode. 
-    op = *((uint16_t *)(datablocks + i*2));
+    op = *((uint16_t *)(datablocks + i));
 //    printf("Op Code: 0x%04x\n", op);
     if (op == OP_NONE){
 //      printf("In OP1 \n");
@@ -71,21 +76,21 @@ int main(){
       outputtobuffer(output);
     } else if (op == OP_RETURN) {
 //      printf("In OP_RETURN \n");
-      uint16_t addr = *((uint16_t *)((uint8_t *)datablocks + (i + 1) * 2));
+      uint16_t addr = *((uint16_t *)(datablocks + (i + 1)));
 //      printf("Addr: %d \n", addr);
       i++; // consume the operand word
 
-      uint16_t output = ((uint8_t *)datablocks)[addr];
+      uint16_t output = ((uint16_t *)datablocks)[addr];
       // The mysterious byte monster has struck again. ITS STILL SHIFTING BY 2 BYTES.
       // SHIFT ERROR FIXED. MEMSET WAS COPYING 5000 BYTES FROM 0 TO 4999. So when i called 5000, it was on 0. but when i used memset to set 5002, and called 5000, memset set the bytes from 0 to 5001. So then, it appeared like there was a shift of 2 bytes. 
       outputtobuffer(&output);
       
     } else if (op == WRITE_CONST_INT){
       
-      uint16_t addr = *((uint16_t *)((uint8_t *)datablocks + (i + 1) * 2));
+      uint16_t addr = *((uint16_t *)(datablocks + (i + 1) ));
       
       i++; // consume the operand word
-      uint16_t value = *((uint16_t *)((uint8_t *)datablocks + (i + 1) * 2));
+      uint16_t value = *((uint16_t *)(datablocks + (i + 1) ));
       i++;
       datablocks[addr] = (uint16_t) value;
       
@@ -96,21 +101,21 @@ int main(){
       
     } else if (op == OP_ADD) {
       i++;
-      uint16_t addr1 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr1 = *((uint16_t *)(datablocks + (i) ));
       i++;
-      uint16_t addr2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr2 = *((uint16_t *)(datablocks + (i) ));
 
-      datablocks[0xFFF1] = datablocks[addr1] + datablocks[addr2];
-      uint16_t * output = malloc(sizeof(uint8_t));
+      datablocks[0xFFF1] = (uint16_t)(datablocks[addr1] + datablocks[addr2]);
+      uint16_t * output = malloc(sizeof(uint16_t));
       *output = (uint16_t)0x0000;
       outputtobuffer(output);
       
     } else if (op == OP_LOAD_REG) {
       
       i++;
-      uint16_t addr1 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr1 = *((uint16_t *)(datablocks + (i) ));
       i++;
-      uint16_t addr2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr2 = *((uint16_t *)(datablocks + (i) ));
       
       datablocks[addr2] = datablocks[addr1];
 
@@ -121,17 +126,17 @@ int main(){
     } else if (op == OP_SUB) {
       
       i++;
-      uint16_t addr1 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr1 = *((uint16_t *)(datablocks + (i)));
       i++;
-      uint16_t addr2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr2 = *((uint16_t *)(datablocks + (i)));
       
       int k = datablocks[addr1] - datablocks[addr2];
       if (k>=0){
-        datablocks[0xFFF2] = k;
+        datablocks[0xFFF3] = (uint16_t)k;
 
       } else {
-        datablocks[0xFFF3] = 1;
-        datablocks[0xFFF2] = k;
+        datablocks[0xFFF5] = 0x0001;
+        datablocks[0xFFF3] = (uint16_t)k;
       }
 
       uint16_t * output = malloc(sizeof(uint16_t));
@@ -141,7 +146,7 @@ int main(){
     } else if (op == OP_JUMP) {
 
       i++;
-      uint16_t addr = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr = *((uint16_t *)(datablocks + (i)));
       i = (addr/2) -1;
       uint16_t * output = malloc(sizeof(uint16_t));
       *output = (uint16_t)0x00;
@@ -149,13 +154,13 @@ int main(){
 
     } else if (op == OP_CMP) {
       i++;
-      uint16_t addr1 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr1 = *((uint16_t *)(datablocks + (i)));
       i++;
-      uint16_t addr2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr2 = *((uint16_t *)(datablocks + (i) ));
       i++;
-      uint16_t jmp1 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t jmp1 = *((uint16_t *)(datablocks + (i)));
       i++;
-      uint16_t jmp2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t jmp2 = *((uint16_t *)(datablocks + (i) ));
       
       if (datablocks[addr1]==datablocks[addr2]){
         i = (jmp1/2) -1;
@@ -165,11 +170,11 @@ int main(){
     } else if (op == OP_CMP_JMP) {
 
       i++;
-      uint16_t addr1 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr1 = *((uint16_t *)(datablocks + (i) ));
       i++;
-      uint16_t addr2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t addr2 = *((uint16_t *)(datablocks + (i)));
       i++;
-      uint16_t jmp = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t jmp = *((uint16_t *)(datablocks + (i) ));
 //      i++;
 //      uint16_t jmp2 = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
       if (datablocks[addr1]==datablocks[addr2]){
@@ -179,11 +184,11 @@ int main(){
     } else if (op == OP_JMP_RELP) {
 
       i++;
-      uint16_t value = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t value = *((uint16_t *)(datablocks + (i) ));
       i = i + (value/2);
     }else if (op == OP_JMP_RELN) {
       i++;
-      uint16_t value = *((uint16_t *)((uint8_t *)datablocks + (i) * 2));
+      uint16_t value = *((uint16_t *)(datablocks + (i)));
 
       i = i - (value/2);
     }else{
